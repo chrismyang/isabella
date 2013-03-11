d64583a86037e493fb44afacb023e6ee90e7c4a6
Chris Yang <chris.m.yang@gmail.com>
2013-03-10 21:53:48 -0700

    Adding another event to the fake event list

--- app/controllers/EventController.scala
+++ app/controllers/EventController.scala
@@ -7,7 +7,11 @@ import play.api.libs.json.{Writes, Json}
 object EventController extends Controller {
   def all = Action {
     val location = Location("55 Music Concourse Dr, San Francisco, CA 94118", 37.7697361, -122.46613809999997)
-    val events = Seq(Event(None, "NightLife at the Academy of Science", location))
+    val event = Event(None, "NightLife at the Academy of Science", location)
+
+    val event2 = Event(None, "Bourbon & Branch", Location("501 Jones St, San Francisco, CA 94102", 37.785826,-122.413016))
+
+    val events = Seq(event, event2)
     Ok(Json.toJson(events)(Writes.seqWrites(Event.Format)))
   }
 }
