d5d2087e0f5406e8d2232c9d30554cbf8009ef10
Chris Yang <chris.m.yang@gmail.com>
2013-03-30 23:27:05 -0700

    Okay, EmailReceiver now reads the message and properly (I hope) parses the content of the body, which was trickier than I would have otherwise thought...

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,12 +3,13 @@ package controllers
 import play.api._
 import play.api.mvc._
 import services.EmailReceiver
+import templates.Html
 
 object Application extends Controller {
   
   def index = Action {
-    new EmailReceiver().doSomething()
-    Ok("")
+    val emails = new EmailReceiver().fetchNewMessages(deleteAfterReading = false)
+    Ok(Html("<ul>%s</ul>".format(emails.map("<li>" + _ + "</li>").mkString("\n"))))
   }
 
   def helloWorld = Action {
--- app/services/EmailReceiver.scala
+++ app/services/EmailReceiver.scala
@@ -1,35 +1,91 @@
 package services
 
-import javax.mail.{MessagingException, NoSuchProviderException, Folder, Session}
+import javax.mail._
+import javax.mail.Flags.Flag
+import java.util.regex.Pattern
 
 class EmailReceiver {
 
-  def doSomething() {
+  def fetchNewMessages(deleteAfterReading: Boolean = true): Seq[Email] = {
+    val session = makeSession
+
+    withStore(session) { store =>
+      val inbox = store.getFolder("Inbox")
+      inbox.open(Folder.READ_WRITE)
+
+      val allMessages = inbox.getMessages.toSeq
+
+      val emails = allMessages map convertMessageToEmail
+
+      if (deleteAfterReading) {
+        makeMessagesAsDeleted(allMessages)
+      }
+
+      inbox.close(true)
+
+      emails
+    }
+  }
+
+  private def makeSession = {
     val props = System.getProperties
     props.setProperty("mail.store.protocol", "imaps")
     val session = Session.getDefaultInstance(props, null)
+    session
+  }
+
+  private def withStore[A](session: Session)(f: Store => A): A = {
     val store = session.getStore("imaps")
     try {
       // use imap.gmail.com for gmail
       store.connect("imap.gmail.com", "isabellas.got.it@gmail.com", "socialsecretary")
-      val inbox = store.getFolder("Inbox")
-      inbox.open(Folder.READ_ONLY)
 
-      // limit this to 20 message during testing
-      val messages = inbox.getMessages()
-      val limit = 20
-      var count = 0
-      for (message <- messages) {
-        count = count + 1
-        if (count > limit) System.exit(0)
-        println(message.getSubject())
-      }
-      inbox.close(true)
-    } catch {
-      case e: NoSuchProviderException =>  e.printStackTrace()
-      case me: MessagingException =>      me.printStackTrace()
+      f(store)
+
     } finally {
       store.close()
     }
   }
+
+  private def convertMessageToEmail(message: Message): Email = {
+    val from = message.getFrom.head
+    val subject = message.getSubject
+
+    val content = parseBodyContent(message)
+
+    Email(from, subject, content)
+  }
+
+  private def makeMessagesAsDeleted(messages: Seq[Message]) {
+    for (message <- messages) {
+      message.setFlags(new Flags(Flag.DELETED), true)
+    }
+  }
+
+  private def parseBodyContent(message: Message): String = {
+    message.getContent match {
+      case mp: Multipart =>
+
+        val parts = for {
+          i <- 0 until mp.getCount
+          bp = mp.getBodyPart(i) if isHtml(bp)
+        } yield {
+          bp.getContent.asInstanceOf[String]
+        }
+
+        parts.mkString("")
+
+      case unknownContent =>
+        "Sorry we couldn't recognize the body of your email, but here's the best we could do:" + unknownContent.toString
+    }
+  }
+
+  private def isHtml(bodyPart: BodyPart): Boolean = {
+    // A little staggered why this is a regex and not a more simple String equality check, but who knows?
+    // http://stackoverflow.com/questions/7212534/read-html-body-of-an-email-using-javamail
+    val BodyPartIsHtmlRegEx = Pattern.compile(Pattern.quote("text/html"), Pattern.CASE_INSENSITIVE)
+    BodyPartIsHtmlRegEx.matcher(bodyPart.getContentType).find()
+  }
 }
+
+case class Email(from: Address, title: String, body: String)
